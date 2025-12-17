89cdf0f75172c5fe1c5d797b209eafbfbe2cd34e
nancy nyokabi <nancynyokabi1995@gmail.com>
2025-12-17 23:55:50 +0300

    Added Course cta component

---
+++ src/components/CourseCTA/CourseCTA.jsx
@@ -0,0 +1,19 @@
+import './CourseCTA.css';
+
+export default function CourseCTA() {
+  return (
+    <section className="course-cta">
+      <div className="cta-card purple">
+        <span>POPULAR COURSES</span>
+        <h3>Get The Best Courses & Upgrade Your Skills</h3>
+        <button>Join With Us</button>
+      </div>
+
+      <div className="cta-card dark">
+        <span>POPULAR COURSES</span>
+        <h3>Get The Best Courses & Upgrade Your Skills</h3>
+        <button>Join With Us</button>
+      </div>
+    </section>
+  );
+}
