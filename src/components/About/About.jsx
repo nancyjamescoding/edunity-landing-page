c25b66fdaaa2b42b91eb7aea0db1a7d25cb3eee5
nancy nyokabi <nancynyokabi1995@gmail.com>
2025-12-18 18:09:23 +0300

    removed the about2 path

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,7 @@ import './About.css';
 import about1 from '@/assets/about1.png';
 import about2 from '@/assets/about2.png';
 import abouticon1 from '@/assets/abouticon1.png';
-import abouticon2 from '@/assets/abouticon2.png';
+// import abouticon2 from '@/assets/abouticon2.png';
 
 export default function About() {
   return (
@@ -42,7 +42,7 @@ export default function About() {
           </div>
 
           <div className="about-feature">
-            <img src={abouticon2} alt="Presenter Control" />
+            {/* <img src={abouticon2} alt="Presenter Control" /> */}
             <div>
               <h4>Presenter Control</h4>
               <p>
